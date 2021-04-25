ed892a205befa2b17b55274e570d4957aa0a788f
fatih40 <baserfatih40@gmail.com>
2021-04-25 02:38:09 +0200

    Added retrofit

--- app/src/main/java/com/fatihbaser/kotlincountrylist/service/CountryApiService.kt
+++ app/src/main/java/com/fatihbaser/kotlincountrylist/service/CountryApiService.kt
@@ -1,4 +1,19 @@
 package com.fatihbaser.kotlincountrylist.service
 
+import com.fatihbaser.kotlincountrylist.model.Country
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class CountryApiService {
+
+        private val BASE_URL="https://raw.githubusercontent.com/"
+    private val api=Retrofit.Builder().baseUrl(BASE_URL ).addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(CountryAPI::class.java)
+
+    fun getData (): Single<List<Country>>{
+        return api.getCoruntries()
+    }
 }
